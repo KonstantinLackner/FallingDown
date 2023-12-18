4aff19c15fd24c54ed20f8d18b624606f690b23e
Shuyin Zheng <61871025+shuyinzh@users.noreply.github.com>
2023-12-19 00:51:56 +0100

    ADD timer & score system + empty star script

--- Assets/Scripts/Cat.cs
+++ Assets/Scripts/Cat.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using TMPro;
 
 public class Cat : MonoBehaviour
 {
@@ -10,6 +11,13 @@ public class Cat : MonoBehaviour
     public SpriteRenderer mySpriteRenderer;
     public List<Sprite> sprites;
     public Transform spawnPoint;
+
+    public TMP_Text timeAliveText;
+    public TMP_Text starsCollectedText;
+    public TMP_Text scoreText;
+    private float timeAlive = 0f;
+    private int starsCollected = 0;
+    private int score = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -21,24 +29,63 @@ public class Cat : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        mySpriteRenderer.flipY = myRigidbody.velocity.y > 0;
-        if (Mathf.Abs(myRigidbody.velocity.y) <= 10.5)
+        if (fallingDown)
         {
-            mySpriteRenderer.sprite = sprites[0];
-        } else if (Mathf.Abs(myRigidbody.velocity.y) >= 10.5 && Mathf.Abs(myRigidbody.velocity.y) <= 15)
-        {
-            mySpriteRenderer.sprite = sprites[1];
-        }
-        else
-        {
-            mySpriteRenderer.sprite = sprites[2];
+            mySpriteRenderer.flipY = myRigidbody.velocity.y > 0;
+            if (Mathf.Abs(myRigidbody.velocity.y) <= 10.5)
+            {
+                mySpriteRenderer.sprite = sprites[0];
+            }
+            else if (Mathf.Abs(myRigidbody.velocity.y) >= 10.5 && Mathf.Abs(myRigidbody.velocity.y) <= 15)
+            {
+                mySpriteRenderer.sprite = sprites[1];
+            }
+            else
+            {
+                mySpriteRenderer.sprite = sprites[2];
+            }
+
+            timeAlive += Time.deltaTime;
+            UpdateScore();
         }
 
         if (Input.GetKey(KeyCode.Space))
         {
-            transform.position = spawnPoint.position;
-            myRigidbody.velocity = Vector2.zero;
-            myRigidbody.angularVelocity = 0f;
+            ResetScore();
+            Respawn();
         }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            Debug.Log("Game over!");
+            fallingDown = false;
+        }
+    }
+
+    private void Respawn()
+    {
+        fallingDown = true;
+        transform.position = spawnPoint.position;
+        myRigidbody.velocity = Vector2.zero;
+        myRigidbody.angularVelocity = 0f;
+    }
+
+    private void UpdateScore()
+    {
+        double timeAliveRounded = System.Math.Round(timeAlive, 1);
+        timeAliveText.text = "Time alive: " + timeAliveRounded + "s";
+        starsCollectedText.text = "Stars collected: " + starsCollected.ToString();
+        score = ((int)timeAliveRounded * 10) + starsCollected * 50;
+        scoreText.text = "Score: " + score;
+    }
+
+    private void ResetScore()
+    {
+        timeAlive = 0;
+        starsCollected = 0;
+        UpdateScore();
     }
 }
