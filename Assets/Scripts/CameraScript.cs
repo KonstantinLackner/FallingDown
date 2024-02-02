92e73d51ce6cef0afe3c8b605857a4ce0d536b4a
Konsti <lackner.konstantin@gamil.com>
2024-02-02 13:43:18 +0100

    ADD new camera test

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f,0f,-10f);
+    private float smoothTime = 0.25f;
+    Vector3 velocity = Vector2.zero;
+    public Transform target;
+    private float highPoint = 0; // It's over, Anakin, I have the high ground!
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.position.y > highPoint)
+        {
+            highPoint = target.position.y;
+        }
+        Vector3 targetPosition = new Vector3(0, highPoint, -10f) + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+}
