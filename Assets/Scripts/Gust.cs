08b4bf6b56143c504da38a992b9d4e41ce6a5ba3
Shuyin Zheng <61871025+shuyinzh@users.noreply.github.com>
2024-02-13 23:32:15 +0100

    UPDATE Gust.cs get components in start

--- Assets/Scripts/Gust.cs
+++ Assets/Scripts/Gust.cs
@@ -6,12 +6,15 @@ public class Gust : MonoBehaviour
 {
     public Transform from;
     public Transform to;
+    private SpriteRenderer mySpriteRenderer;
+    private BoxCollider2D myBoxCollider2D;
 
     private float timer = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        mySpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        myBoxCollider2D = gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -20,12 +23,12 @@ public class Gust : MonoBehaviour
         timer += Time.deltaTime;
         if (timer is > 3f and < 4)
         {
-            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 0);
-            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            mySpriteRenderer.color = new Color(1, 1, 1, 0);
+            myBoxCollider2D.enabled = false;
         } else if (timer > 4f)
         {
-            gameObject.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1);
-            gameObject.GetComponent<BoxCollider2D>().enabled = true;
+            mySpriteRenderer.color = new Color(1, 1, 1, 1);
+            myBoxCollider2D.enabled = true;
             timer = 0;
         }
     }
