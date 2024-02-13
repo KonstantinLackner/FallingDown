855c97ad92e7dcd510ad3c964cfe5b2cd237e681
Konsti <lackner.konstantin@gamil.com>
2024-02-13 17:02:25 +0100

    UPDATE start screen

--- Assets/Scripts/CloudMovement.cs
+++ Assets/Scripts/CloudMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour
+public class CloudMovement : MonoBehaviour
 {
     public float speed;
 
