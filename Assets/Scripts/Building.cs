d3e13796dd0905faffc9bae9a7809c6ee91979c9
Konsti <lackner.konstantin@gamil.com>
2024-02-13 21:18:57 +0100

    UPDATE H2 prefab

--- Assets/Scripts/Building.cs
+++ Assets/Scripts/Building.cs
@@ -1,6 +1,14 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Building : MonoBehaviour
 {
     public SpriteRenderer houseSpriteRenderer;
+
+    private void Awake()
+    {
+        houseSpriteRenderer.color = new Color(Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));;
+    }
 }
