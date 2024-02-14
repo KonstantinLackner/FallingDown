a57cd1b91d6adab62c15d70cf1bf1b729d6aab1a
Shuyin Zheng <61871025+shuyinzh@users.noreply.github.com>
2024-02-14 02:06:09 +0100

    UPDATE unlocking items with growth of progress bar

--- Assets/Scripts/ProgressBar.cs
+++ Assets/Scripts/ProgressBar.cs
@@ -9,6 +9,7 @@ public class ProgressBar : MonoBehaviour
     private ProgressTracker progressTracker;
     public AudioSource countAudioSource;
     public AudioSource progressBackingTrackAudioSource;
+    public AudioSource unlockAudioSource;
     public TMP_Text scoreText;
     public TMP_Text highscoreText;
     public TMP_Text currentStarText;
@@ -34,30 +35,36 @@ public class ProgressBar : MonoBehaviour
             highscoreText.text = " highscore\n" + progressTracker.highscore.Item1 + " meters" + " | " + progressTracker.highscore.Item2 + " stars";
         }
 
+        // for debugging
+        // progressTracker.previousStarCount = 23;
+        // progressTracker.starCount = 42;
+
         currentStarText.text = progressTracker.starCount + "/60";
 
-        List<GameObject> unlockedItems = GetUnlockedItems(progressTracker.starCount);
+        oldValue = MapValue(progressTracker.previousStarCount);
+        transform.localScale = new Vector3(oldValue, transform.localScale.y, 1);
+        List<GameObject> unlockedItems = GetUnlockedItems(progressTracker.previousStarCount);
         foreach (GameObject item in unlockedItems)
         {   
             item.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
             item.transform.GetChild(0).gameObject.SetActive(true);
         }
 
-        oldValue = MapValue(progressTracker.previousStarCount, 0f, 60f, 0f, 14.5f);
-        transform.localScale = new Vector3(oldValue, transform.localScale.y, 1);
-
-        newValue = MapValue(progressTracker.starCount, 0f, 60f, 0f, 14.5f);
+        newValue = MapValue(progressTracker.starCount);
     }
 
     void Update()
     {
         if (updateTick > 0.15f) 
         {
+            float xValue = transform.localScale.x;
             float yValue = transform.localScale.y;
-            if (transform.localScale.x <= newValue)
+            if (xValue <= newValue)
             {
                 countAudioSource.Play();
-                transform.localScale = new Vector3(transform.localScale.x + 0.15f, yValue, 1);
+                float nextXValue = transform.localScale.x + 0.2f;
+                TryUnlockingItem(xValue, nextXValue);
+                transform.localScale = new Vector3(nextXValue, yValue, 1);
             }
             updateTick = 0;
         }
@@ -69,8 +76,49 @@ public class ProgressBar : MonoBehaviour
 
     }
 
-    private float MapValue(float oldValue, float oldMin, float oldMax, float newMin, float newMax)
+    private void TryUnlockingItem(float position, float nextPosition)
     {
+
+        GameObject itemToUnlock = null;
+        if (position <= MapValue(10) && nextPosition >= MapValue(10))
+        {
+            itemToUnlock = unlockableItems[1];
+        }        
+        if (position <= MapValue(20) && nextPosition >= MapValue(20))
+        {
+            itemToUnlock = unlockableItems[2];
+        }        
+        if (position <= MapValue(30) && nextPosition >= MapValue(30))
+        {
+            itemToUnlock = unlockableItems[3];
+        }        
+        if (position <= MapValue(40) && nextPosition >= MapValue(40))
+        {
+            itemToUnlock = unlockableItems[4];
+        }        
+        if (position <= MapValue(50) && nextPosition >= MapValue(50))
+        {
+            itemToUnlock = unlockableItems[5];
+        }        
+        if (position <= MapValue(60) && nextPosition >= MapValue(60))
+        {
+            itemToUnlock = unlockableItems[6];
+        }
+
+        if (itemToUnlock != null)
+        {
+            unlockAudioSource.Play();
+            itemToUnlock.GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, 255);
+            itemToUnlock.transform.GetChild(0).gameObject.SetActive(true);
+        }
+    }
+
+    private float MapValue(float oldValue)
+    {
+        float oldMin = 0;
+        float oldMax = 60f;
+        float newMin = 0f;
+        float newMax = 14.5f;
         return ((oldValue - oldMin) / (oldMax - oldMin)) * (newMax - newMin) + newMin;
     }
 
